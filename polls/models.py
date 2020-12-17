a602800b0b145538e1085dec387e407a728b19e8
medteuw <medteuw@gmail.com>
2020-12-17 18:05:17 +0000

    partie-7 is done

--- polls/models.py
+++ polls/models.py
@@ -17,6 +17,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.date_pub <= now
+    was_published_recently.admin_order_field = 'date_pub'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
